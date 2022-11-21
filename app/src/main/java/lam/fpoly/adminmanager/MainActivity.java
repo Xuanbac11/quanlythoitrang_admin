053b62f9cd5511f02294662f91358212d1c8fdaa
Duy Lam <111013821+Duylamjunpio@users.noreply.github.com>
2022-11-21 13:54:35 +0700

    edit admin

--- app/src/main/java/lam/fpoly/adminmanager/MainActivity.java
+++ app/src/main/java/lam/fpoly/adminmanager/MainActivity.java
@@ -1,10 +1,15 @@
 package lam.fpoly.adminmanager;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,6 +17,7 @@ import android.view.View;
 
 import android.widget.TextView;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.learnoset.material.ui.learnosetnavigationbar.CustomNavTheme;
 import com.learnoset.material.ui.learnosetnavigationbar.LearnosetNavItem;
 import com.learnoset.material.ui.learnosetnavigationbar.LearnosetNavigationBar;
@@ -23,25 +29,39 @@ import lam.fpoly.adminmanager.Fragment.QuanLySanPham;
 public class MainActivity extends AppCompatActivity{
     private DrawerLayout drawerLayout;
     private LearnosetNavigationBar learnosetNavigationBar;
-    private boolean checkColor = false;
+    private boolean checkColor = true;
+    private Toolbar toolbar;
+    private TextView mTitle;
+    private AppBarLayout actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Drawable drawable= getResources().getDrawable(R.drawable.ic_baseline_menu_24);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(drawable);
-
         drawerLayout = findViewById(R.id.drawerLayout);
-        learnosetNavigationBar = findViewById(R.id.navigatioNabr);
+        toolbar = findViewById(R.id.tbToolBar);
 
+        setSupportActionBar(toolbar);//add toolbar vào ứng dụng
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAE8E0")));
+        mTitle = toolbar.findViewById(R.id.toolbar_title);
+        mTitle.setText("Quản lý sản phẩm");
+        mTitle.setTextColor(Color.parseColor("#A45C40"));
+
+        //thêm thanh 3 gạch vào toolbar
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,R.string.opendrawer,R.string.closedrawer);
+        toggle.syncState();
+
+        learnosetNavigationBar = findViewById(R.id.navigatioNabr);
         learnosetNavigationBar.setDrawerLayout(drawerLayout, LearnosetNavigationBar.DrawerGravity.LEFT);
 
         settingCustomIcons();
         settingCustomGroup();
         settingCustomColorLight();
+        settingProfile();
 
         learnosetNavigationBar.setEventListener(new NavigationEventListener() {
             @Override
@@ -49,37 +69,63 @@ public class MainActivity extends AppCompatActivity{
                 switch (position){
                     case 0:
                         selectedNavItem.setFragment(new QuanLySanPham(),R.id.fragmentContainer);
-                        getSupportActionBar().setTitle("Quản lý sản phẩm");
+                        mTitle.setText("Quản lý sản phẩm");
+                        mTitle.setTextColor(Color.parseColor("#A45C40"));
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAE8E0")));
                         break;
                     case 1:
                         //Quản lý đơn hàng
-                        getSupportActionBar().setTitle("Quản lý đơn hàng");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("Quản lý đơn hàng");
+                        mTitle.setTextColor(Color.parseColor("#A45C40"));
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAE8E0")));
                         break;
                     case 2:
                         //Quản lý khuyến mại
-                        getSupportActionBar().setTitle("Quản lý khuyến mại");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("Quản lý khuyến mại");
+                        mTitle.setTextColor(Color.parseColor("#A45C40"));
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAE8E0")));
                         break;
                     case 3:
                         //Quản lý khách hàng
-                        getSupportActionBar().setTitle("Quản lý khách hàng");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("Quản lý khách hàng");
+                        mTitle.setTextColor(Color.parseColor("#A45C40"));
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FAE8E0")));
                         break;
                     case 4:
                         //Quản lý đánh giá
-                        getSupportActionBar().setTitle("Feedback");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("FeedBack");
+                        mTitle.setTextColor(Color.WHITE);
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#385C9A")));
                         break;
                     case 5:
                         //Quản lý tài khoản
-                        getSupportActionBar().setTitle("Quản lý tài khoản");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("Quản lý tài khoản");
+                        mTitle.setTextColor(Color.WHITE);
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#4C5270")));
                         break;
                     case 6:
                         //Thống kê
-                        getSupportActionBar().setTitle("Thống kê");
+                        //set Fragment tương ứng vào đây
+                        //selectedNavItem.setFragment(new 'Tên Fragment',R.id.fragmentContainer);
+                        mTitle.setText("Thống kê");
+                        mTitle.setTextColor(Color.WHITE);
+                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#145DA0")));
                         break;
                     case 7:
-                        if(checkColor == false){
-                            settingCustomColorLight();
-                        }else {
+                        if(checkColor){
                             settingCustomColorDark();
+                        }else {
+                            settingCustomColorLight();
                         }
                         break;
                 }
@@ -87,11 +133,17 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onLogOutBtnClick() {
+                //Khoi tao lai Activity main
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
 
+                // Tao su kien ket thuc app
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startActivity(startMain);
+                finish();
             }
         });
-
-        settingProfile();
 
     }
 
@@ -103,6 +155,7 @@ public class MainActivity extends AppCompatActivity{
         customNavTheme.setSelectedItemIconColor(Color.WHITE);
         customNavTheme.setSelectedItemTextColor(Color.WHITE);
         customNavTheme.setTextColor(Color.WHITE);
+        customNavTheme.setGroupNameColor(Color.WHITE);
 
         learnosetNavigationBar.setTheme(customNavTheme);
 
@@ -141,23 +194,22 @@ public class MainActivity extends AppCompatActivity{
         learnosetNavigationBar.addNavItem(customItem2);
         learnosetNavigationBar.addNavItem(customItem3);
         learnosetNavigationBar.addNavItem(customItem4);
-        learnosetNavigationBar.addNavItem(LearnosetNavItem.BuiltInItems.FEEDBACK);
     }
 
     private void settingCustomGroup(){
+
+        NavItemsGroup navItemsGroup3 = new NavItemsGroup("FeedBack");
+        navItemsGroup3.addGroupItem(LearnosetNavItem.BuiltInItems.FEEDBACK);
+        learnosetNavigationBar.addItemsGroup(navItemsGroup3);
+
         NavItemsGroup navItemsGroup = new NavItemsGroup("Account & Statistical");
-
         navItemsGroup.addGroupItem(new LearnosetNavItem("Quản lý tài khoản", R.drawable.account_circle_outline));
-
         LearnosetNavItem customGroupItem = new LearnosetNavItem("Thống kê", R.drawable.clipboard_data);
         navItemsGroup.addGroupItem(customGroupItem);
-
         learnosetNavigationBar.addItemsGroup(navItemsGroup);
 
         NavItemsGroup navItemsGroup2 = new NavItemsGroup("Color Menu");
-
         navItemsGroup2.addGroupItem(LearnosetNavItem.BuiltInItems.TOOLS);
-
         learnosetNavigationBar.addItemsGroup(navItemsGroup2);
     }
 
